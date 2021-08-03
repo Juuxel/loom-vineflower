44c0a5d8d1aa534c93346295bef53eee4beadc93
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-08-04 01:27:06 +0300

    tests: Clean up replacements

--- src/test/java/juuxel/loomquiltflower/test/BasicProjectTest.java
+++ src/test/java/juuxel/loomquiltflower/test/BasicProjectTest.java
@@ -5,28 +5,13 @@ import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BasicProjectTest extends ProjectTest {
-    private static Map<String, String> getReplacements() {
-        return Map.of("LOOM_ID", System.getProperty("loomId", "fabric-loom"));
-    }
-
     @Test
-    void test() throws IOException {
+    void test() {
         // Set up
         setupProject("basic");
-        String buildGradle = getProjectFileText("basic", "build.gradle");
-
-        for (var entry : getReplacements().entrySet()) {
-            buildGradle = buildGradle.replace('@' + entry.getKey() + '@', entry.getValue());
-        }
-
-        Files.writeString(projectDirectory.toPath().resolve("build.gradle"), buildGradle);
 
         // Run
         BuildResult result = GradleRunner.create()
--- src/test/java/juuxel/loomquiltflower/test/ProjectTest.java
+++ src/test/java/juuxel/loomquiltflower/test/ProjectTest.java
@@ -9,13 +9,19 @@ import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 abstract class ProjectTest {
     @TempDir
     protected File projectDirectory;
 
+    protected Map<String, Map<String, String>> getReplacements() {
+        return Map.of("build.gradle", Map.of("LOOM_ID", System.getProperty("loomId", "fabric-loom")));
+    }
+
     protected final void setupProject(String name, String... extraFiles) {
         try {
+            copyProjectFile(name, "build.gradle");
             copyProjectFile(name, "settings.gradle");
 
             for (String file : extraFiles) {
@@ -41,10 +47,22 @@ abstract class ProjectTest {
     }
 
     private void copyProjectFile(String projectName, String from, String to) throws IOException {
-        try (InputStream in = getProjectFile(projectName, from)) {
-            Path target = projectDirectory.toPath().resolve(to);
-            Files.createDirectories(target.getParent());
-            Files.copy(in, target);
+        Path target = projectDirectory.toPath().resolve(to);
+        Files.createDirectories(target.getParent());
+        var replacements = getReplacements();
+
+        if (replacements.containsKey(from)) {
+            String text = getProjectFileText(projectName, from);
+
+            for (var entry : replacements.get(text).entrySet()) {
+                text = text.replace('@' + entry.getKey() + '@', entry.getValue());
+            }
+
+            Files.writeString(target, text);
+        } else {
+            try (InputStream in = getProjectFile(projectName, from)) {
+                Files.copy(in, target);
+            }
         }
     }
 }
