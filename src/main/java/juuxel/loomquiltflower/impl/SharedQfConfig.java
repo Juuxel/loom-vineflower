9b05bebabeac1350271e4a3067e79d3f8ae549f6
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-01-23 13:48:36 +0200

    Fix numberOfThreads being an int instead of a string

--- src/main/java/juuxel/loomquiltflower/impl/SharedQfConfig.java
+++ src/main/java/juuxel/loomquiltflower/impl/SharedQfConfig.java
@@ -11,7 +11,7 @@ public final class SharedQfConfig {
         options.put(IFernflowerPreferences.BYTECODE_SOURCE_MAPPING, "1");
         options.put(IFernflowerPreferences.REMOVE_SYNTHETIC, "1");
         options.put(IFernflowerPreferences.LOG_LEVEL, "trace");
-        options.put(IFernflowerPreferences.THREADS, ReflectionUtil.getFieldOrRecordComponent(metadata, "numberOfThreads"));
+        options.put(IFernflowerPreferences.THREADS, String.valueOf(ReflectionUtil.<Integer>getFieldOrRecordComponent(metadata, "numberOfThreads")));
         options.putAll(ReflectionUtil.<Map<String, String>>maybeGetFieldOrRecordComponent(metadata, "options").orElse(Map.of()));
     }
 }
