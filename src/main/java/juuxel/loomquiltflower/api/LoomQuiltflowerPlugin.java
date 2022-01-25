3656401c73cb6125643d1b67906620b11574cb45
Juuxel <6596629+Juuxel@users.noreply.github.com>
2022-01-25 19:58:54 +0200

    Revert "Prefer Arch decompiler API over Loom 0.11's API"
    This reverts commit a0cc6100a40c9f9514fafc8b5a9e748dad068b99.

--- src/main/java/juuxel/loomquiltflower/api/LoomQuiltflowerPlugin.java
+++ src/main/java/juuxel/loomquiltflower/api/LoomQuiltflowerPlugin.java
@@ -34,10 +34,10 @@ public class LoomQuiltflowerPlugin implements Plugin<Project> {
             target.getPluginManager().withPlugin(loomId, p -> {
                 String moduleClass;
 
-                if (isNewArchLoom()) {
-                    moduleClass = "juuxel.loomquiltflower.impl.module.ArchLoomSetup";
-                } else if (isNewLoom()) {
+                if (isNewLoom()) {
                     moduleClass = "juuxel.loomquiltflower.impl.module.Loom011Setup";
+                } else if (isNewArchLoom()) {
+                    moduleClass = "juuxel.loomquiltflower.impl.module.ArchLoomSetup";
                 } else if (isOldLoom()) {
                     moduleClass = "juuxel.loomquiltflower.impl.module.OldLoomSetup";
                 } else {
