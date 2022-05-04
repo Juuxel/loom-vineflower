c8528d931cd38e5c3a0c7dcb0b4567ae165dee76
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-05-04 17:03:19 +0300

    Remove plugin portal publishing from GH Actions [ci skip]

--- setup-plugin-portal-login.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "project.ext.set('gradle.publish.key', '$GRADLE_PUBLISH_KEY')" > private.gradle
-echo "project.ext.set('gradle.publish.secret', '$GRADLE_PUBLISH_SECRET')" >> private.gradle
