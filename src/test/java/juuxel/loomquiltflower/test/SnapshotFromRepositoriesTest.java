a5060c9ffd874c392aa003beeba48c4f90275e05
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-05-27 20:53:28 +0300

    Add test for QF snapshot versions from project repositories

---
+++ src/test/java/juuxel/loomquiltflower/test/SnapshotFromRepositoriesTest.java
@@ -0,0 +1,28 @@
+package juuxel.loomquiltflower.test;
+
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SnapshotFromRepositoriesTest extends ProjectTest {
+    @Test
+    void test() {
+        // Set up
+        setupProject("snapshot-from-repositories");
+
+        // Run
+        BuildResult result = GradleRunner.create()
+            .withPluginClasspath()
+            .withProjectDir(projectDirectory)
+            .withArguments("help", "--stacktrace")
+            .forwardOutput()
+            .withDebug(true)
+            .build();
+
+        assertThat(result.getOutput()).contains("Resolved: 1.9.0-20221030.213144-213");
+        assertThat(result.task(":help").getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
+    }
+}
